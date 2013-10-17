d53ba5c702d39ffb198949d055defa3d5c0d0fb8
Diego Williams Aguilar Montaño <alu0100592368@ull.edu.es>
2013-10-18 00:37:58 +0100

    creado el constructor y el metodo initialize

--- lib/racional.rb
+++ lib/racional.rb
@@ -1,11 +1,28 @@
 # Clase para crear objetos racionales
 
-require "./gcd.rb"
-require "./lcm.rb"
+require "./lib/gcd.rb"
+require "./lib/lcm.rb"
 
 class Fraccion
-  #Método getter para las variables @a y @b
-  attr_reader :a, :b
+  
+  # Constructor de la clase, si recibe 2 argumentos el primero
+  # es el numerador y el segundo el denominador. 
+  # Si recibe 1 es el numerador con denominador 1.
+  def initialize(*args)
+    if args.size == 2    
+      x = mcd(args[0],args[1])
+      @a = args[0] / x
+      @b = args[1] / x
+    else
+      @a = args[0]
+      @b = 1
+    end
+  end
 
+  # Devuelve una cadena con la representación del racional
+  def to_s
+    "#{@a}/#{@b}"
+  end
+  
   
 end
