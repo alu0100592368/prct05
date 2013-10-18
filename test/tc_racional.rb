900d7a18699d865fdd86a6fc98aa54e368c5d51b
Diego Williams Aguilar Montaño <alu0100592368@ull.edu.es>
2013-10-18 01:28:36 +0100

    añadiendo primera prueba correcta para suma

--- test/tc_racional.rb
+++ test/tc_racional.rb
@@ -1,8 +1,13 @@
-# Implementar en este fichero las Pruebas Unitarias asociadas a la clase Fraccion
-
-require "./racional.rb"
+# Pruebas Unitarias asociadas a la clase Fraccion
+require "./lib/racional.rb"
 require "test/unit"
 
 class Test_Fraccion < Test::Unit::TestCase
+  def test_suma
+  	assert_equal([5, 1], Fraccion.new(2).suma(3))        # (2) + (3) = (5)
+    assert_equal([7, 1], Fraccion.new(4,1).suma(3,1))  # (3/5) + (1/2) = (11/10)
+    assert_equal([17, 6], Fraccion.new(5,2).suma(2,6))   # (5/2) + (2/6) = (17/6)
+    assert_equal([4, 1], Fraccion.new(15,5).suma(5,5))   # (15/5) + (5/5) = (4)
+  end
 
 end
